cf9e4a3329d37ed46c58e767ab1a0cb16b3aba40
kagepurinman <kagepurinman111@gmail.com>
2013-07-12 16:44:50 +0900

    all switch bugs is clean

--- src/main/java/org/bukkit/material/TrapDoor.java
+++ src/main/java/org/bukkit/material/TrapDoor.java
@@ -7,84 +7,86 @@ import org.bukkit.block.BlockFace;
  * Represents a trap door
  */
 public class TrapDoor extends SimpleAttachableMaterialData implements Openable {
-    public TrapDoor() {
-        super(Material.TRAP_DOOR);
+  public TrapDoor() {
+    super(Material.TRAP_DOOR);
+  }
+
+  public TrapDoor(final int type) {
+    super(type);
+  }
+
+  public TrapDoor(final Material type) {
+    super(type);
+  }
+
+  public TrapDoor(final int type, final byte data) {
+    super(type, data);
+  }
+
+  public TrapDoor(final Material type, final byte data) {
+    super(type, data);
+  }
+
+  public boolean isOpen() {
+    return ((getData() & 0x4) == 0x4);
+  }
+
+  public void setOpen(boolean isOpen) {
+    byte data = getData();
+
+    if (isOpen) {
+      data |= 0x4;
+    } else {
+      data &= ~0x4;
     }
 
-    public TrapDoor(final int type) {
-        super(type);
+    setData(data);
+  }
+
+  /**
+   * Test if trapdoor is inverted
+   * 
+   * @return true if inverted (top half), false if normal (bottom half)
+   */
+  public boolean isInverted() {
+    return ((getData() & 0x8) != 0);
+  }
+
+  /**
+   * Set trapdoor inverted state
+   * 
+   * @param inv - true if inverted (top half), false if normal (bottom half)
+   */
+  public void setInverted(boolean inv) {
+    int dat = getData() & 0x7;
+    if (inv) {
+      dat |= 0x8;
+    }
+    setData((byte) dat);
+  }
+
+  public BlockFace getAttachedFace() {
+    byte data = (byte) (getData() & 0x3);
+
+    switch (data) {
+      case 0x0:
+        return BlockFace.SOUTH;
+
+      case 0x1:
+        return BlockFace.NORTH;
+
+      case 0x2:
+        return BlockFace.EAST;
+
+      case 0x3:
+        return BlockFace.WEST;
     }
 
-    public TrapDoor(final Material type) {
-        super(type);
-    }
+    return null;
 
-    public TrapDoor(final int type, final byte data) {
-        super(type, data);
-    }
+  }
 
-    public TrapDoor(final Material type, final byte data) {
-        super(type, data);
-    }
-
-    public boolean isOpen() {
-        return ((getData() & 0x4) == 0x4);
-    }
-
-    public void setOpen(boolean isOpen) {
-        byte data = getData();
-
-        if (isOpen) {
-            data |= 0x4;
-        } else {
-            data &= ~0x4;
-        }
-
-        setData(data);
-    }
-
-    /**
-     * Test if trapdoor is inverted
-     * @return true if inverted (top half), false if normal (bottom half)
-     */
-    public boolean isInverted() {
-        return ((getData() & 0x8) != 0);
-    }
-
-    /**
-     * Set trapdoor inverted state
-     * @param inv - true if inverted (top half), false if normal (bottom half)
-     */
-    public void setInverted(boolean inv) {
-        int dat = getData() & 0x7;
-        if (inv) {
-            dat |= 0x8;
-        }
-        setData((byte) dat);
-    }
-
-    public BlockFace getAttachedFace() {
-        byte data = (byte) (getData() & 0x3);
-
-        switch (data) {
-            case 0x0:
-                return BlockFace.SOUTH;
-
-            case 0x1:
-                return BlockFace.NORTH;
-
-            case 0x2:
-                return BlockFace.EAST;
-
-            case 0x3:
-                return BlockFace.WEST;
-        }
-
-        return null;
-
-    }
-
-    public void setFacingDirection(BlockFace face) {
+  public void setFacingDirection(BlockFace face) {
         byte data = (byte) (getData() & 0xC);
 
         switch (face) {
@@ -97,18 +99,20 @@ public class TrapDoor extends SimpleAttachableMaterialData implements Openable {
             case EAST:
                 data |= 0x3;
                 break;
+                default:breakl;
         }
 
         setData(data);
     }
 
-    @Override
-    public String toString() {
-        return (isOpen() ? "OPEN " : "CLOSED ") + super.toString() + " with hinges set " + getAttachedFace() + (isInverted() ? " inverted" : "");
-    }
+  @Override
+  public String toString() {
+    return (isOpen() ? "OPEN " : "CLOSED ") + super.toString() + " with hinges set "
+        + getAttachedFace() + (isInverted() ? " inverted" : "");
+  }
 
-    @Override
-    public TrapDoor clone() {
-        return (TrapDoor) super.clone();
-    }
+  @Override
+  public TrapDoor clone() {
+    return (TrapDoor) super.clone();
+  }
 }
